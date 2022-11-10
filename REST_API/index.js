3fb8f5ae9956b2ba0e211b1866ef90469ce2bc76
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-10 21:24:10 +0200

    Create index.js

---
+++ REST_API/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const port =  process.env.PORT || "3030"
+const db = require("./config/datebase");
+ const auth = require("./middlewares/isAuth");
+const router = require("./config/router");
+const cors = require("./middlewares/cors");
+const app = express();
+
+startServer();
+
+async function startServer() {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cors());
+  app.use(auth());
+  app.use(router)
+
+  db();
+  app.listen(port, () => console.log(`Server  is running on port ${port} (:`));
+}
