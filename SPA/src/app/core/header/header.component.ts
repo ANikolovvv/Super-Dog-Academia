ef23100137cfe843832c7fc88734985aae31acca
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-12 21:33:24 +0200

    Update header.component.ts

--- SPA/src/app/core/header/header.component.ts
+++ SPA/src/app/core/header/header.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   imageAlt = 'dog';
-  
+  guest=true;
+  user=false;
   constructor() { }
 
   ngOnInit(): void {
