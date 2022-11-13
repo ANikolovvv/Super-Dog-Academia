f7f59518ffec5eafabadbdf4c377f5b81f2f2c8b
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-13 20:38:18 +0200

    Update app.module.ts

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { CoachesComponent } from './coaches/coaches.component';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { GalleryComponent } from './gallery/gallery.component';
     AppComponent,
     HomeComponent,
     GalleryComponent,
+    CoachesComponent,
 
   ],
   imports: [
