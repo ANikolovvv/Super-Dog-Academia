e140e82e62c8d409c655a7641ba72e954af323d7
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-13 20:37:55 +0200

    Update trainerServices.js

--- REST_API/services/trainerServices.js
+++ REST_API/services/trainerServices.js
@@ -1,7 +1,7 @@
 const Trainer = require("../models/Trainer");
 
 exports.getAllTrainers = async () => {
-  return await Comics.find({}).lean();
+  return await Trainer.find({}).lean();
 };
 exports.createData = async (order) => {
   let newOrder = await Trainer.create(order);
@@ -9,23 +9,15 @@ exports.createData = async (order) => {
   return newOrder;
 };
 exports.getById = async (id) => {
-  const comic = await Trainer.findById(id);
+  const trainer = await Trainer.findById(id);
 
-  if (!comic) {
+  if (!trainer) {
     throw new Error("Sory someting when wrong!");
   }
-  return comic;
+  return trainer;
 };
 exports.deleteData = async (id) => {
   return await Trainer.findByIdAndDelete(id);
 };
 
-exports.comicsAddLike = async (user, id) => {
-  const comic = await Trainer.findById(id).populate("userLiked");
-  if (!comic) {
-    throw new Error("Sory someting when wrong!");
-  }
-  comic.userLiked.push(user);
-  comic.save();
-  return comic;
-};
+
