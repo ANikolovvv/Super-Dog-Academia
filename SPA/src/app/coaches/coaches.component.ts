40a175c3d1c31cdf15a999716a0531776fcfa6c3
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-14 14:01:49 +0200

    Update coaches.component.ts

--- SPA/src/app/coaches/coaches.component.ts
+++ SPA/src/app/coaches/coaches.component.ts
@@ -1,22 +1,54 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { apiServer } from '../app-service';
 import { ICoaches } from '../interfaces/coaches';
+import { trigger, transition, style, animate } from '@angular/animations';
+
+const enterTransition = transition(':enter', [
+  style({
+    opacity: 0,
+   
+  }),
+  animate('1s ease-in', style({
+    opacity: 1,
+    background: ('#00ff04'),
+    
+  }))
+])
+const exitTransition = transition(':leave', [
+  style({
+    opacity: 1,
+    background: ('red'),
+    
+
+  }),
+  animate("1s ease-out", style({
+    opacity: 0,
+    
+  }))
+])
+const loader = trigger('loader', [enterTransition]);
+const loaderOut = trigger('loaderOut', [exitTransition]);
 
 @Component({
   selector: 'app-coaches',
   templateUrl: './coaches.component.html',
-  styleUrls: ['./coaches.component.scss']
+  styleUrls: ['./coaches.component.scss'],
+  animations:[loader,loaderOut]
 })
 export class CoachesComponent implements OnInit {
   coaches: ICoaches[] | null = null;
   errors = false;
-  btn = "show";
+  btn = false;
+  loading=false;
 
   constructor(private apiServer: apiServer) { }
-
+  animation(){
+    this.loading=!this.loading;
+  }
   ngOnInit(): void {
     this.apiServer.loadCoaches().subscribe({
       next: (value) => {
+        
         this.coaches = value;
 
       },
@@ -27,17 +59,15 @@ export class CoachesComponent implements OnInit {
     })
   }
   @HostListener('window:scroll', ['$event'])
-  // onScroll(e:any) {
-  //   console.log('window', e.currentTarget,1+6);
-  // }
+ 
 
   divScroll(e: any) {
-
-    this.btn = "hidden";
-    setTimeout(() => {
-      this.btn = "show"
-    }, 2000)
-
+     this.btn=true;
+     setTimeout(() => {
+     this.btn = false;
+   }, 8000)
+   
   }
+
 
 }
