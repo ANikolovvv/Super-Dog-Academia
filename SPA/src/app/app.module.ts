5ba4b7ca83b7dbc10cd27bb166a41022fd7024ec
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-14 14:01:40 +0200

    Update app.module.ts

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
@@ -9,6 +10,11 @@ import { HomeComponent } from './home/home.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { CoachesComponent } from './coaches/coaches.component';
 
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { OrderComponent } from './order/order.component';
+import { CoursesComponent } from './courses/courses.component';
+
 
 @NgModule({
   declarations: [
@@ -16,13 +22,18 @@ import { CoachesComponent } from './coaches/coaches.component';
     HomeComponent,
     GalleryComponent,
     CoachesComponent,
+    RegisterComponent,
+    LoginComponent,
+    OrderComponent,
+    CoursesComponent,
 
   ],
   imports: [
     BrowserModule,
     CoreModule,
     HttpClientModule,
-  
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
