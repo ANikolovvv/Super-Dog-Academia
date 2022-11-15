e9006dd335a9e56becdff7801ef138de02702c58
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-15 12:19:47 +0200

    add routing

--- SPA/src/app/app-routing.module.ts
+++ SPA/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { LogoutComponent } from './auth/logout/logout.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { CoachesComponent } from './coaches/coaches.component';
+import { CoursesComponent } from './courses/courses.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path: 'gallery',component:GalleryComponent},
+  {path:"courses",component:CoursesComponent},
+  {path:"coaches",component:CoachesComponent},
+  {path:"auth/register",component:RegisterComponent},
+  {path:"auth/login",component:LoginComponent},
+  {path:"auth/logout",component:LogoutComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
