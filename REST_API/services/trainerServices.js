ce4902c42ca18e7fe86e1c867f8e487c712a4c1d
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-16 16:20:40 +0200

    Update trainerServices.js

--- REST_API/services/trainerServices.js
+++ REST_API/services/trainerServices.js
@@ -1,10 +1,19 @@
 const Trainer = require("../models/Trainer");
+const Courses=require("../models/Courses")
 
 exports.getAllTrainers = async () => {
   return await Trainer.find({}).lean();
 };
+exports.getAllCourses = async () => {
+  return await Courses.find({}).lean();
+};
 exports.createData = async (order) => {
   let newOrder = await Trainer.create(order);
+
+  return newOrder;
+};
+exports.createCours = async (order) => {
+  let newOrder = await Courses.create(order);
 
   return newOrder;
 };
@@ -15,6 +24,14 @@ exports.getById = async (id) => {
     throw new Error("Sory someting when wrong!");
   }
   return trainer;
+};
+exports.getByIdCourses = async (id) => {
+  const cours = await Courses.findById(id);
+
+  if (!cours) {
+    throw new Error("Sory someting when wrong!");
+  }
+  return cours;
 };
 exports.deleteData = async (id) => {
   return await Trainer.findByIdAndDelete(id);
