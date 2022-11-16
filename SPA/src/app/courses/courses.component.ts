ce0f666518f87f11c277013195ad1e13f63ab8be
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-16 16:21:03 +0200

    Update courses.component.ts

--- SPA/src/app/courses/courses.component.ts
+++ SPA/src/app/courses/courses.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from "@angular/common";
+import { apiServer } from '../app-service';
+import { ICourses } from '../interfaces/courses';
 
 @Component({
   selector: 'app-courses',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
-
-  constructor() { }
+  courses: ICourses[] | null = null;
+  errors = false;
+  btn = true;
+  loading = true;
+  constructor(private apiServer: apiServer, @Inject(DOCUMENT)
+  private document: Document) { }
 
   ngOnInit(): void {
+    this.apiServer.loadCourses().subscribe({
+      next: (value) => {
+        this.loading = !this.loading;
+        this.courses = value;
+
+      },
+      error: (err) => {
+        this.errors = !this.loading;
+        console.error(err);
+      }
+    })
+
   }
 
 }
