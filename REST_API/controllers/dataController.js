5485dfa14e228a676c0962ddf6a8b5c851d52b88
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-16 16:19:39 +0200

    Update dataController.js

--- REST_API/controllers/dataController.js
+++ REST_API/controllers/dataController.js
@@ -2,16 +2,15 @@ const router = require("express").Router();
 
 const api = require("../services/trainerServices");
 
-router.get("/",async (req,res)=>{
+router.get("/", async (req, res) => {
   try {
-    let data = await api.getAllTrainers()
+    let data = await api.getAllTrainers();
 
     res.json(data);
   } catch (error) {
     res.status(400).json({ message: "Bad request" });
   }
-
-})
+});
 router.post("/", async (req, res) => {
   const item = {
     name: req.body.name,
@@ -21,6 +20,31 @@ router.post("/", async (req, res) => {
   };
   try {
     let result = await api.createData(item);
+    res.status(201).json({ result });
+  } catch (error) {
+    res.status(400).json({ message: "Bad request" });
+    console.log(error);
+  }
+});
+router.get("/cours", async (req, res) => {
+  try {
+    let data = await api.getAllCourses();
+
+    res.json(data);
+  } catch (error) {
+    res.status(400).json({ message: "Bad request" });
+  }
+});
+router.post("/cours", async (req, res) => {
+  const item = {
+    title: req.body.title,
+    training: req.body.training,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    desc: req.body.desc
+  };
+  try {
+    let result = await api.createCours(item);
     res.status(201).json({ result });
   } catch (error) {
     res.status(400).json({ message: "Bad request" });
