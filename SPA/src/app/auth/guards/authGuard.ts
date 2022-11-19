45cb68f915292c34644206ac6c1485798085239a
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-19 18:04:25 +0200

    Create guard

---
+++ SPA/src/app/auth/guards/authGuard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+
+import { Observable } from 'rxjs';
+import { AuthService } from '../auth.service';
+//import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+    constructor(private router: Router, private authServer: AuthService) {
+
+    }
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        const { authCuard, authRedirect } = route.data;
+        if (typeof authCuard === 'boolean' && authCuard === this.authServer.isUser) {
+            return true
+        }
+        return  this.router.parseUrl(authRedirect) || '/'
+    }
+  
+
+}
