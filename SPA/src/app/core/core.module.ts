9305a006e7b89e35e8cae3f3dbc578f0b6fdafe4
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-19 18:05:06 +0200

    Update core.module.ts

--- SPA/src/app/core/core.module.ts
+++ SPA/src/app/core/core.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser, isPlatformServer } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-import {FooterComponent} from "./footer/footer.component";
-import { RouterModule} from '@angular/router';
-import {routes} from "../app-routing.module";
+import { FooterComponent } from "./footer/footer.component";
+import { RouterModule } from '@angular/router';
 import { LoaderComponent } from './loader/loader.component';
 import { ButtonUpComponent } from './button-up/button-up.component'
+import { AuthService } from '../auth/auth.service';
+import { AuthGuard } from '../auth/guards/authGuard';
+import { LocalStorage } from './storage-inject';
 
 
 
@@ -27,7 +29,44 @@ import { ButtonUpComponent } from './button-up/button-up.component'
     RouterModule,
     LoaderComponent,
     ButtonUpComponent
-    
-  ]
+
+  ],
+  providers: [AuthService, AuthGuard, {
+    provide: LocalStorage,
+    useFactory: (platformId: Object) => {
+      if (isPlatformBrowser(platformId)) {
+        return window.localStorage
+      }
+      if (isPlatformServer(platformId)) {
+        return class implements Storage {
+
+          length = 0;
+          private data: Record<string, string> = {};
+          clear(): void {
+            this.data = {};
+          }
+          getItem(key: string): string | null {
+            return this.data[key];
+          }
+          key(index: number): string | null {
+            throw new Error('Method not implemented.');
+          }
+          removeItem(key: string): void {
+            const { [key]: removeItem, ...other } = this.data;
+            this.data = other;
+          }
+          setItem(key: string, value: string): void {
+            this.data[key] = value;
+          }
+
+        }
+      }
+      throw new Error('No working')
+
+    },
+    deps: [(PLATFORM_ID)]
+
+  }]
+
 })
 export class CoreModule { }
