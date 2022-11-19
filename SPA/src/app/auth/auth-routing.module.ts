fe18af1cd3c31979cb9661ca636a467af02bff19
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-19 18:03:21 +0200

    Create auth routing

---
+++ SPA/src/app/auth/auth-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {  AuthGuard } from './guards/authGuard';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+export const routes: Routes = [
+   {
+      path: "auth/login",
+      component: LoginComponent,
+      canActivate:[AuthGuard],
+      data: {
+         authCuard: false,
+         authRedirect: '/'
+      }
+   },
+   {
+      path: "auth/register",
+      component: RegisterComponent,
+      canActivate:[AuthGuard],
+      data: {
+         authCuard: false,
+         authRedirect: '/'
+      }
+   },
+];
+
+@NgModule({
+   imports: [RouterModule.forChild(routes)],
+   exports: [RouterModule]
+})
+export class AuthRoutingModule { }
