a27ed78ee69dd4fddec1539adeb2d37a0031cba7
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-19 18:05:01 +0200

    Update register.component.ts

--- SPA/src/app/auth/register/register.component.ts
+++ SPA/src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService:AuthService, private router:Router) { }
 
   ngOnInit(): void {
   }
-
+  register(email:string,password:string):void{
+    this.authService.register(email,password)
+    this.router.navigate(['/courses']);
+}
 }
