398d7012982b068da9afb8de4f8bd388d56a3c5e
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-23 21:13:17 +0200

    Update login.component.ts

--- SPA/src/app/auth/login/login.component.ts
+++ SPA/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
@@ -8,14 +9,19 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  
-  constructor(private authService:AuthService,private router:Router) { }
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
-  login(email:string,password:string):void{
-      this.authService.login(email,password);
+  handleLogin(form: NgForm): void {
+    if (form.invalid) { return; }
+    const value: { email: string; password: string } = form.value;
+    this.authService.login(value.email,value. password);
+    form.setValue({ email: '', password: '' });
+    console.log(value);
+    
 
-      this.router.navigate(['/courses']);
+    this.router.navigate(['/courses']);
   }
 }
