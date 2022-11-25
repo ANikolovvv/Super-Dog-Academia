cd56489f8e4077498e78202ff925c3bc40253db3
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-25 21:52:19 +0200

    Update register.component.ts

--- SPA/src/app/auth/register/register.component.ts
+++ SPA/src/app/auth/register/register.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, NgForm, Validators } from '@angular/forms';
+import { emailValidator, passwordValidator } from '../validators';
 
 @Component({
   selector: 'app-register',
@@ -10,17 +11,27 @@ import { NgForm } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(private authService:AuthService, private router:Router) { }
+  form = this.fb.group({
+    email: ['', [Validators.required,emailValidator(['bg','com'])]],
+    pass:this.fb.group({
+      password:['', [Validators.required, Validators.minLength(4)]],
+      rePass:[]
+    },{
+      validators:[passwordValidator('password','rePass')]
+    })
+  })
+  constructor(private authService: AuthService, private router: Router, private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
-  register(form:NgForm):void{
-    if (form.invalid) { return; }
-    const value: { email: string; password: string; repass:string } = form.value;
-    this.authService.register(value.email,value. password);
-    form.setValue({ email: '', password: '' ,repass:''});
-    this.router.navigate(['/courses']);
-    console.log(value);
-   
-}
+  register(): void {
+     console.log(this.form.value)
+    // if (form.invalid) { return; }
+    // const value: { email: string; password: string; repass: string } = form.value;
+    // this.authService.register(value.email, value.password);
+    // form.setValue({ email: '', password: '', repass: '' });
+    // this.router.navigate(['/courses']);
+    // console.log(value);
+
+  }
 }
