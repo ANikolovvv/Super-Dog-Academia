1f27cbf9317c98ef0b67fc147b46fe9b0db7358d
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-11-26 16:52:35 +0200

    Update password-validator.ts

--- SPA/src/app/auth/validators/password-validator.ts
+++ SPA/src/app/auth/validators/password-validator.ts
@@ -6,6 +6,6 @@ export function passwordValidator(password: string, rePass: string): ValidatorFn
         const pass1 = group.get(password);
         const pass2 = group.get(rePass)
 
-        return pass1?.value === pass2?.value ? null : { asswordValidator: true };
+        return pass1?.value === pass2?.value ? null : { passwordValidator: true };
     }
 }
