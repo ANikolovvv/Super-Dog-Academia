0ad0bcdf3ad7cc1f3de02632bd547f309d956994
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-04 18:15:11 +0200

    Update auth.service.ts

--- SPA/src/app/auth/auth.service.ts
+++ SPA/src/app/auth/auth.service.ts
@@ -5,6 +5,7 @@ import { BehaviorSubject, catchError, filter, Observable, Subscription, tap, thr
 import { environment } from 'src/environments/environment';
 import { LocalStorage } from '../core/storage-inject';
 import { IAuth } from '../interfaces/auth';
+import { IOrder } from '../interfaces/course';
 import { IUser } from '../interfaces/user';
 const url = environment.url;
 
@@ -19,10 +20,10 @@ export class AuthService implements OnDestroy {
   );
   auth: IAuth | undefined;
   user: IUser | null = null;
-
   error: any;
-  token:any;
-
+  token: any;
+  guest: any;
+  
   get isUser() {
     return this.user !== null;
   }
@@ -31,74 +32,98 @@ export class AuthService implements OnDestroy {
       'Content-Type': 'application/json'
     })
   }
-   
-
 
   subscription: Subscription;
-  constructor(@Inject(LocalStorage) private localeStorage: Window['localStorage'],private https: HttpClient) {
+  constructor(@Inject(LocalStorage) private localeStorage: Window['localStorage'], private https: HttpClient) {
     this.subscription = this.user$.subscribe(user => {
       this.user = user;
     });
+
     try {
       const local = this.localeStorage.getItem('<USER>') || 'Error';
       this.user = JSON.parse(local)
-      console.log('is user',  local)
+
     } catch (error) {
       this.user = null;
-      //console.log(error)
+     
     }
   }
 
-  login(email: string, password: string, rePass: string): Observable<IUser>  {
+  login(email: string, password: string, rePass: string): Observable<IUser> {
     this.user = {
       email,
       password,
       rePass
 
     }
-    this.localeStorage.setItem('<USER>', JSON.stringify(this.user))
-    return this.https.post<IUser>(`${url}/auth/login`,JSON.stringify({email,password,rePass}),this.httpOptions)
-    .pipe(tap(user => this.user$$.next(user)));
+    //this.localeStorage.setItem('<USER>', JSON.stringify(this.user))
+    return this.https.post<IUser>(`${url}/auth/login`, JSON.stringify({ email, password, rePass }), this.httpOptions)
+      .pipe(tap(user => this.user$$.next(user)));
   }
-  createToken(token:any){
-    return this.auth=token;
+  createToken(token: any) {
+    this.auth = token;
+    this.token = token.accessToken
+    this.localeStorage.setItem('<USER>', JSON.stringify(token))
+    console.log('token')
+    return token;
   }
 
   register(ctx: any): Observable<IUser> {
-   
     this.user = ctx;
-    
-    this.localeStorage.setItem('<USER>', JSON.stringify(this.user))
     return this.https.post<IUser>(`${url}/auth/register`, JSON.stringify(ctx), this.httpOptions)
-    .pipe(tap(user => this.user$$.next(user))
-    )
+      .pipe(tap(user => this.user$$.next(user))
+      )
 
   }
-  
+
   logout() {
-    this.token=this.auth?.accessToken;
-   const httpTokenOptions=  {
-      headers:new HttpHeaders({
+    const httpTokenOptions = {
+      headers: new HttpHeaders({
         "Content-Type": "application/json",
         "X-Authorization": this.token
       })
-      
+
     }
-    console.log(this.auth)
-    this.user=null
+    this.user = null
     this.localeStorage.clear();
-    return this.https.get<IUser>(`${url}/auth/logout`,httpTokenOptions)
-    
+    return this.https.get<IUser>(`${url}/auth/logout`, httpTokenOptions)
+
   }
 
-  errorHandler(error: any) {
-    let errorMessage = '';
-    if (error.error instanceof ErrorEvent) {
-      errorMessage = error.error.message;
-    } else {
-      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+  getMyCourse(): Observable<IOrder> {
+    this.guest = this.localeStorage.getItem('<USER>')
+    const ctx = this.guest;
+    const id = JSON.parse(ctx)._id;
+    return this.https.get<IOrder>(`${url}/trainer/my-data/${id}`);
+  }
+  deleteMyCourse(id: any): Observable<IOrder> {
+
+    this.guest = this.localeStorage.getItem('<USER>')
+    const ctx = this.guest;
+    const accessToken = JSON.parse(ctx).accessToken;
+    console.log(accessToken, 'acc')
+    const httpTokenOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        "X-Authorization": accessToken
+      })
+
     }
-    return throwError(errorMessage);
+    return this.https.delete<IOrder>(`${url}/trainer/my-data/${id}`, httpTokenOptions);
+  }
+  createMyCourse(ctx: any): Observable<IOrder> {
+    this.guest = this.localeStorage.getItem('<USER>')
+    const info = this.guest;
+    const token = JSON.parse(info).accessToken;
+    console.log(token, 'create')
+    const httpTokenOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        "X-Authorization": token
+      })
+
+    }
+    return this.https.post<IOrder>(`${url}/trainer/my-data`, JSON.stringify(ctx), httpTokenOptions)
   }
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
