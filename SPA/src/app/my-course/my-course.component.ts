c4d36331b53df451a29378621c1bc5e2a60de881
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-04 18:15:59 +0200

    Create my-course.component.ts

---
+++ SPA/src/app/my-course/my-course.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { IOrder } from '../interfaces/course';
+import { ICourses } from '../interfaces/courses';
+
+@Component({
+  selector: 'app-my-course',
+  templateUrl: './my-course.component.html',
+  styleUrls: ['./my-course.component.scss']
+})
+export class MyCourseComponent implements OnInit {
+
+
+  courses: IOrder[] | null = null;
+  arr: any;
+  errors = false;
+  info: boolean = true;
+  loading = true;
+
+  constructor(private authServer: AuthService, private router: Router) { }
+  ngOnInit(): void {
+
+    this.authServer.getMyCourse().subscribe({
+      next: (value: any) => {
+        this.info = value.history.length > 0;
+        this.loading = !this.loading;
+        this.courses = value.history;
+        this.arr = value.history
+        console.log(this.info, 'fdddfgfdfdgdffdgd')
+
+      },
+      error: (err: any) => {
+        this.errors = !this.loading;
+        console.error(err);
+      }
+    })
+
+  }
+
+  deleteHandler(id: any): void {
+    this.authServer.deleteMyCourse(id).subscribe({
+      next: (value: any) => {
+        console.log(value, ' delete  fdddfgfdfdgdffdgd')
+        this.router.navigate(['/my-course']);
+      },
+      error: (err: any) => {
+        console.error(err);
+      }
+    })
+    console.log(id, 'delete')
+  }
+
+
+
+}
