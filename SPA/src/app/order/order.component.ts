171f595f38b01db7731852a540e0ffb17a5a3bdd
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-04 18:16:02 +0200

    Update order.component.ts

--- SPA/src/app/order/order.component.ts
+++ SPA/src/app/order/order.component.ts
@@ -3,6 +3,7 @@ import { apiServer } from '../app-service';
 import { IOrder } from '../interfaces/course';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-order',
@@ -17,8 +18,8 @@ export class OrderComponent implements OnInit {
   id: string = '';
   ngSelect: string = 'male';
 
-  constructor(private apiServer: apiServer,
-    private route: ActivatedRoute) { }
+  constructor(private authService: AuthService,private apiServer:apiServer,
+    private route: ActivatedRoute,private path:Router) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(param => {
@@ -43,6 +44,25 @@ export class OrderComponent implements OnInit {
       name: string, breed: string,
       age: number, gender: string
     } = form.value;
-    console.log(value)
+     const data={
+        email:form.value.email,
+        name:form.value.name,
+        age:form.value.age,
+        breed:form.value.breed,
+        gender:form.value.gender,
+        tel:form.value.tel,
+        title:this.course?.title,
+        desc:this.course?.desc,
+        imageUrl:this.course?.imageUrl,
+        training:this.course?.training,
+        price:this.course?.price
+     }
+    console.log(data,'asdasdasasasasaa')
+     this.authService.createMyCourse(data).subscribe((res:any)=>{
+          console.log('Course created successfully',res);
+     })
+   
+    
+    this.path.navigate(['/my-course']);
   }
 }
