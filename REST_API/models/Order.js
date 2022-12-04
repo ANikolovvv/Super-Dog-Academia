6ac8031c88d434be1f8911ac5b3aabb1039a5967
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-04 18:14:54 +0200

    Update Order.js

--- REST_API/models/Order.js
+++ REST_API/models/Order.js
@@ -4,8 +4,14 @@ const orderSchema = new mongoose.Schema({
   name: { type: String, required: [true, "All fields are required"] },
   breed: { type: String, required: true },
   email: { type: String, required: true },
-  phone:{type:Number,required:true},
-  price: { type: Number, default: true },
+  phone:{type:String,required:true},
+  gender:{type:String,required:true},
+  training: [],
+  title: { type: String, default: true },
+  desc: { type: String, default: true },
+  imageUrl: { type: String, default: true },
+  age: { type: Number, default: true },
+  price:{type:Number,default:true},
   _ownerId: { type: mongoose.Types.ObjectId, ref: "User" },
 });
 ///    const ctx = { title, author, email, address,courier,payment };
