70ee521a1286c0da2d30e12c5b53208d8ed2f1d6
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-04 18:15:44 +0200

    Create order.ts

---
+++ SPA/src/app/interfaces/order.ts
@@ -0,0 +1,14 @@
+export interface IOrder {
+    title:string,
+    email:string,
+    breed:string,
+    age:string,
+    gender:string,
+    tel:string,
+    name: string,
+    training: string[],
+    imageUrl: string,
+    price: number,
+    desc: string
+
+}
