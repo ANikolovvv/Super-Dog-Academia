c601e9a3b819f7e09ab8b484f5b0348a724d0b8a
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-07 21:20:10 +0200

    Create actions

---
+++ SPA/src/app/+store/actions.ts
@@ -0,0 +1,11 @@
+import { createAction, props } from "@ngrx/store";
+
+const actioTypes = {
+    loadCourse: 'CREATE_COURSE',
+    loadCourseSuccess: 'LOAD_COURSE_SUCCESS',
+    loadCourseFailure: 'LOAD_COURSE_FAILURE',
+};
+
+export const loadCourse = createAction(actioTypes.loadCourse);
+export const loadCorseSuccess = createAction(actioTypes.loadCourseSuccess,props<{ courses: any }>());
+export const loadCourseFailure = createAction(actioTypes.loadCourseFailure,props<{ error: any }>());
