9403a02cb5f1334103c8a5ec253e7472ce6bd9d7
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-07 21:20:59 +0200

    Update app.module.ts

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -1,36 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, isDevMode } from '@angular/core';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-
-import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { CoachesComponent } from './coaches/coaches.component';
 
-
 import { OrderComponent } from './order/order.component';
 import { CoursesComponent } from './courses/courses.component';
-import { PreloadAllModules, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { routes } from './app-routing.module';
 import { AuthModule } from './auth/auth.module';
 import { apiServer } from './app-service';
 import { AuthGuard } from './auth/guards/authGuard';
-import { FormsModule } from '@angular/forms';
+import { Effect } from './+store/effect';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { AppInterceptor } from './app-interceptor';
 import { MyCourseComponent } from './my-course/my-course.component';
+
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { reducers } from './+store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
+   AppComponent,
     HomeComponent,
     GalleryComponent,
     CoachesComponent,
@@ -38,22 +42,23 @@ import { EffectsModule } from '@ngrx/effects';
     CoursesComponent,
     NotFoundComponent,
     MyCourseComponent,
-    
-  ],
+],
 
   imports: [
     BrowserModule,
     CoreModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    FormsModule,AuthModule,
+    FormsModule, AuthModule,
     RouterModule.forRoot(routes),
-    StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([]),
-   
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([Effect]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
     
+
+
   ],
-  providers: [apiServer, AuthGuard,AppInterceptor],
+  providers: [apiServer, AuthGuard, AppInterceptor],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
