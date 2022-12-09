aa9c4a527408a3d3fc912ecab5e093c00e734ad3
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-09 19:05:22 +0200

    Update order.component.ts

--- SPA/src/app/order/order.component.ts
+++ SPA/src/app/order/order.component.ts
@@ -4,6 +4,10 @@ import { IOrder } from '../interfaces/course';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../auth/auth.service';
+import { Actions } from '@ngrx/effects';
+import { Store } from '@ngrx/store';
+import { getHistory } from '../+store/selectors';
+import { addCourse } from '../+store/actions';
 
 @Component({
   selector: 'app-order',
@@ -18,8 +22,12 @@ export class OrderComponent implements OnInit {
   id: string = '';
   ngSelect: string = 'male';
 
-  constructor(private authService: AuthService,private apiServer:apiServer,
-    private route: ActivatedRoute,private path:Router) { }
+  course$ = this.store.select(getHistory);
+  constructor(private authService: AuthService, private apiServer: apiServer,
+    private route: ActivatedRoute, private path: Router,
+    private actions$: Actions,
+    private store: Store
+  ) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(param => {
@@ -44,25 +52,37 @@ export class OrderComponent implements OnInit {
       name: string, breed: string,
       age: number, gender: string
     } = form.value;
-     const data={
-        email:form.value.email,
-        name:form.value.name,
-        age:form.value.age,
-        breed:form.value.breed,
-        gender:form.value.gender,
-        tel:form.value.tel,
-        title:this.course?.title,
-        desc:this.course?.desc,
-        imageUrl:this.course?.imageUrl,
-        training:this.course?.training,
-        price:this.course?.price
-     }
-    console.log(data,'asdasdasasasasaa')
-     this.authService.createMyCourse(data).subscribe((res:any)=>{
-          console.log('Course created successfully',res);
-     })
-   
+    const data = {
+      email: form.value.email,
+      name: form.value.name,
+      age: form.value.age,
+      breed: form.value.breed,
+      gender: form.value.gender,
+      tel: form.value.tel,
+      title: this.course?.title,
+      desc: this.course?.desc,
+      imageUrl: this.course?.imageUrl,
+      training: this.course?.training,
+      price: this.course?.price
+    }
+    console.log(data, 'asdasdasasasasaa')
     
+    // this.course$.subscribe({
+    //   next: (value: any | null) => {
+
+    //     console.log(value, 'order')
+    //   },
+    //   error: (err: any) => {
+    //     this.errors = !this.loading;
+    //     console.error(err);
+    //   }
+    // })
+    this.authService.createMyCourse(data).subscribe((res: any) => {
+      console.log('Course created successfully', res);
+      this.store.dispatch(addCourse(res.result))
+    })
+
+
     this.path.navigate(['/my-course']);
   }
 }
