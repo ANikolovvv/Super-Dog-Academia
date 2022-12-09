8d2c39ebd3f0124172659a7e5e895f6714538eae
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-09 19:05:12 +0200

    Update index.ts

--- SPA/src/app/+store/index.ts
+++ SPA/src/app/+store/index.ts
@@ -1,29 +1,43 @@
 import { ActionReducerMap, createReducer, on } from "@ngrx/store";
 import { routerReducer } from '@ngrx/router-store';
-import { loadCorseSuccess } from "./actions";
+import { addCourse, loadCorseSuccess, loadCourse } from "./actions";
 
 export interface IMainState {
     courses: null | any[];
-    
+    history:any
+
 }
 
-interface IAppState {
+export interface IAppState {
     main: IMainState;
     router: ReturnType<typeof routerReducer>
 }
 const mainInitialState: IMainState = {
     courses: null,
-   
+    history:null
+
 }
 const mainReducer = createReducer<IMainState>(
     mainInitialState,
-   
-    on(loadCorseSuccess, (state:any, { courses}:any) => {
-        console.log({courses},'state')
-        return { ...state, courses };
-    })
-)
 
+    on(loadCorseSuccess, (state: any, { courses }: any) => {
+      
+        return { ...state,courses,history:courses,};
+    }),
+    on(addCourse, (state: any, value:any) => {
+        const {history}=state
+        let num=0
+        console.log(state , 'addCoursestate')
+        console.log(value, 'addCorsevalue',history)
+        return { ...state,history:[history,value]};
+    }),
+  
+   
+)
+// on(addTodo, (state, { content }) => ({
+//     ...state,
+//     todos: [...state.todos, { id: Date.now().toString(), content: content }],
+//   })),
 export const reducers: ActionReducerMap<IAppState> = {
     main: mainReducer,
     router: routerReducer,
