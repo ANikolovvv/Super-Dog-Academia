b350aff7f02b446ce04d2cb7dfe08873575eef85
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-09 19:05:06 +0200

    Update actions.ts

--- SPA/src/app/+store/actions.ts
+++ SPA/src/app/+store/actions.ts
@@ -1,11 +1,14 @@
 import { createAction, props } from "@ngrx/store";
+import { IOrder } from "../interfaces/order";
 
 const actioTypes = {
     loadCourse: 'CREATE_COURSE',
     loadCourseSuccess: 'LOAD_COURSE_SUCCESS',
     loadCourseFailure: 'LOAD_COURSE_FAILURE',
+    addCourse: "ADD_COURSE"
 };
 
 export const loadCourse = createAction(actioTypes.loadCourse);
-export const loadCorseSuccess = createAction(actioTypes.loadCourseSuccess,props<{ courses: any }>());
-export const loadCourseFailure = createAction(actioTypes.loadCourseFailure,props<{ error: any }>());
+export const loadCorseSuccess = createAction(actioTypes.loadCourseSuccess,props<{ courses: any}>());
+export const loadCourseFailure = createAction(actioTypes.loadCourseFailure, props<{ error: any }>());
+export const addCourse = createAction(actioTypes.addCourse,props<{ data: any }>());
