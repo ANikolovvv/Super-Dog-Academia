aa9d244ab4547c73b5f01ded108d54de8dc8f58e
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-10 15:51:50 +0200

    Update register.component.ts

--- SPA/src/app/auth/register/register.component.ts
+++ SPA/src/app/auth/register/register.component.ts
@@ -9,7 +9,7 @@ import { IAuth } from 'src/app/interfaces/auth';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss','./responsive.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
