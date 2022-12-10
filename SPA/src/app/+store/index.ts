9ed0bb5a5970e4a37dde599b3be10031f92ee566
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-10 18:59:59 +0200

    Update index.ts

--- SPA/src/app/+store/index.ts
+++ SPA/src/app/+store/index.ts
@@ -4,7 +4,7 @@ import { addCourse, loadCorseSuccess, loadCourse } from "./actions";
 
 export interface IMainState {
     courses: null | any[];
-    history:any
+    history: any
 
 }
 
@@ -14,26 +14,26 @@ export interface IAppState {
 }
 const mainInitialState: IMainState = {
     courses: null,
-    history:null
+    history: null
 
 }
 const mainReducer = createReducer<IMainState>(
     mainInitialState,
 
     on(loadCorseSuccess, (state: any, { courses }: any) => {
-               console.log(courses,'Load courses')
-        return { ...state,courses,history:courses,};
+        console.log(courses, 'Load courses')
+        return { ...state, courses, history: courses, };
     }),
-    on(addCourse, (state: any, value:any) => {
-        const {history}=state;
-        const {courses}=state;
-        
-        console.log(courses , 'addCoursestate',state)
-        console.log(value, 'addCorsevalue',history)
-        return { ...state,courses:[courses,value],history:[history,value]};
+    on(addCourse, (state: any, value: any) => {
+        const { history } = state;
+        const { courses } = state;
+
+        console.log(courses, 'addCoursestate', state)
+        console.log(value, 'addCorsevalue', history)
+        return { ...state, courses: [courses, value], history: [history, value] };
     }),
-  
-   
+
+
 )
 
 export const reducers: ActionReducerMap<IAppState> = {
