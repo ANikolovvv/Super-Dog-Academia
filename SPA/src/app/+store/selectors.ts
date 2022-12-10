8cccc93aed6b93d2ec84e5ae9d8243987728a705
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-10 19:00:02 +0200

    Update selectors.ts

--- SPA/src/app/+store/selectors.ts
+++ SPA/src/app/+store/selectors.ts
@@ -4,7 +4,3 @@ import { IMainState } from './index';
 const mainSelector = createFeatureSelector<IMainState>('main');
 export const selectHistory =createSelector (mainSelector,s=> s.history);
 export const getHistory = createSelector(mainSelector, s => s.courses);
-// export const selectAllTodos = createSelector(
-//     selectTodos,
-//     (state: TodoState) => state.todos
-//   );
