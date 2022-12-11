30e7b78a91e3683c4a7f2a0c7e4ac554376583a3
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-11 17:49:12 +0200

    Update dataController.js

--- REST_API/controllers/dataController.js
+++ REST_API/controllers/dataController.js
@@ -1,10 +1,8 @@
 const router = require("express").Router();
 const preload = require("../middlewares/preload");
 const api = require("../services/trainerServices");
-const userApi=require("../services/userServices");
-const orderApi=require("../services/orderServices");
-
-
+const userApi = require("../services/userServices");
+const orderApi = require("../services/orderServices");
 
 router.get("/", async (req, res) => {
   try {
@@ -45,7 +43,7 @@ router.post("/course", async (req, res) => {
     training: req.body.training,
     imageUrl: req.body.imageUrl,
     price: req.body.price,
-    desc: req.body.desc
+    desc: req.body.desc,
   };
   try {
     let result = await api.createCours(item);
@@ -69,25 +67,24 @@ router.get("/my-data/:id", async (req, res) => {
   }
 });
 router.post("/my-data", async (req, res) => {
-  
   const item = {
-    name:req.body.name,
-    title:req.body.title,
-    training:req.body.training,
-    imageUrl:req.body.imageUrl,
+    name: req.body.name,
+    title: req.body.title,
+    training: req.body.training,
+    imageUrl: req.body.imageUrl,
     email: req.body.email,
     phone: req.body.tel,
     breed: req.body.breed,
     age: Number(req.body.age),
     price: Number(req.body.price),
     gender: req.body.gender,
-    desc:req.body.desc,
+    desc: req.body.desc,
     _ownerId: req.user._id,
   };
-  console.log('dsda',item)
+
   try {
     let result = await orderApi.createData(item);
-    console.log('dsda')
+
     res.status(201).json({ result });
   } catch (error) {
     res.status(400).json({ message: "Bad request" });
@@ -97,7 +94,7 @@ router.post("/my-data", async (req, res) => {
 
 router.delete("/my-data/:id", async (req, res) => {
   const id = req.params.id;
-  console.log(id,'ok delete')
+  console.log(id, "ok delete");
   try {
     const result = await orderApi.deleteData(id);
     res.json({ result });
