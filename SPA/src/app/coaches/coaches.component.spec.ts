fd1324e8fd7e8704e8ebed88f255d0da74d6fe10
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-13 20:42:29 +0200

    add more unit testing

--- SPA/src/app/coaches/coaches.component.spec.ts
+++ SPA/src/app/coaches/coaches.component.spec.ts
@@ -1,16 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of } from 'rxjs';
+import { apiServer } from '../app-service';
+import { LoaderComponent } from '../core/loader/loader.component';
+import { ICoaches } from '../interfaces/coaches';
 import { CoachesComponent } from './coaches.component';
 
 describe('CoachesComponent', () => {
   let component: CoachesComponent;
   let fixture: ComponentFixture<CoachesComponent>;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
+  let service: apiServer;
+
 
   beforeEach(async () => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new apiServer(httpClientSpy)
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     await TestBed.configureTestingModule({
-      declarations: [ CoachesComponent ]
+      declarations: [CoachesComponent],
+      providers: [apiServer]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(CoachesComponent);
     component = fixture.componentInstance;
@@ -20,4 +34,38 @@ describe('CoachesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('#get coaches should return array',
+
+    (done: DoneFn) => {
+      const expectedCourses: ICoaches[] =
+        [{
+          name: 'rex',
+          description: 'best',
+          imageUrl: 'image',
+          year: 10
+        },
+        {
+          name: 'roky',
+          description: 'best',
+          imageUrl: 'image',
+          year: 10
+        }];
+
+      httpClientSpy.get.and.returnValue(of(expectedCourses));
+
+      service.loadCoaches().subscribe({
+        next: courses => {
+          expect(courses)
+            .withContext('expected coaches')
+            .toEqual(expectedCourses);
+          done();
+        },
+        error: done.fail
+      });
+      expect(httpClientSpy.get.calls.count())
+        .withContext('one call')
+        .toBe(1);
+    });
 });
+
+
