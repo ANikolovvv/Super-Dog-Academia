db9311f31a702f9649936006542b2706fc78c309
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-15 21:57:14 +0200

    Update header.component.ts

--- SPA/src/app/core/header/header.component.ts
+++ SPA/src/app/core/header/header.component.ts
@@ -1,14 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, group, keyframes, state, style, transition, trigger } from '@angular/animations';
+import { Component, MissingTranslationStrategy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss', './responsive.component.scss']
+  styleUrls: ['./header.component.scss', './responsive.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({ transform: 'translateY(0)' })),
+      transition('void => *', [
+        style({ transform: 'translateY(-100%)' }),
+        animate(1000)
+      ]),
+      transition('* => void', [
+        animate(1000, style({ transform: 'translateY(100%)'}))
+      ])
+    ]),
+    trigger('run', [
+      state('in', style({ transform: 'translateX(0)' })),
+      transition('void => *', [
+        style({ transform: 'translateX(-100%)' }),
+        animate(1000)
+      ]),
+      transition('* => void', [
+        animate(1000, style({ transform: 'translateX(100)'}))
+      ])
+    ]),
+  ]
+                             
 })
+
 export class HeaderComponent implements OnInit {
   imageAlt = 'super-dog';
+  isOpen: boolean = true;
+  
   get isLogged() {
     return this.auhtService.isUser;
   }
@@ -22,7 +49,10 @@ export class HeaderComponent implements OnInit {
 
   }
   ngOnInit(): void {
-    console.log('guest', this.guest)
+    setInterval(() => {
+      this.isOpen = !this.isOpen;
+     
+    }, 5000)
   }
   logout() {
     console.log('logaut', this.auhtService.user)
