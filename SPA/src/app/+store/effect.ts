adf2013c2e754c3afbc2872acd003f4283023654
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-16 21:28:21 +0200

    Update effect.ts

--- SPA/src/app/+store/effect.ts
+++ SPA/src/app/+store/effect.ts
@@ -1,31 +1,29 @@
 import { Injectable } from "@angular/core";
+
 import { createEffect, Actions, ofType } from '@ngrx/effects';
-import {  Store } from "@ngrx/store";
-import { catchError,  map,  switchMap } from "rxjs";
+import { Store } from "@ngrx/store";
+import { catchError,  map, switchMap} from "rxjs";
 import { IAppState } from ".";
 import { AuthService } from "../auth/auth.service";
-import { addCourse, loadCorseSuccess, loadCourse, loadCourseFailure } from "./actions";
+import { loadCorseSuccess, loadCourse, loadCourseFailure } from "./actions";
 
 @Injectable({
     providedIn: 'root'
 })
 
-
 export class Effect {
 
-    createCourse = createEffect(() =>
+    getCourse = createEffect(() =>
 
         this.actions$.pipe(
             ofType(loadCourse),
             switchMap(
                 () => this.auth.getMyCourse().pipe(
-                    map(courses => loadCorseSuccess({ courses })),
+                    map(courses => loadCorseSuccess({courses})),
                     catchError(error => [loadCourseFailure({ error })])
                 ))
         ))
-    constructor(private actions$: Actions, private auth: AuthService, private store: Store<IAppState>,) {
-
-    }
-
-
+    
+     
+    constructor(private actions$: Actions, private auth: AuthService, private store: Store<IAppState>) { }
 }
