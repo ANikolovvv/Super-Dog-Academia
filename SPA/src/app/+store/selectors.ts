186f6e11e577cd3d5059dc3c343ca76065e3e46e
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-16 21:28:28 +0200

    Update selectors.ts

--- SPA/src/app/+store/selectors.ts
+++ SPA/src/app/+store/selectors.ts
@@ -2,5 +2,5 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { IMainState } from './index';
 
 const mainSelector = createFeatureSelector<IMainState>('main');
-export const selectHistory =createSelector (mainSelector,s=> s.history);
+
 export const getHistory = createSelector(mainSelector, s => s.courses);
