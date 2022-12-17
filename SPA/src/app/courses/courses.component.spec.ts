753db202d8f569fae7e4504cf165d40a8c88a476
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-17 17:33:59 +0200

    Update courses.component.spec.ts

--- SPA/src/app/courses/courses.component.spec.ts
+++ SPA/src/app/courses/courses.component.spec.ts
@@ -1,9 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule, } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
 import { apiServer } from '../app-service';
-import { ICoaches } from '../interfaces/coaches';
 import { ICourses } from '../interfaces/courses';
 
 
