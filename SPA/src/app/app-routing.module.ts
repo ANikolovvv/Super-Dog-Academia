ff8bf95b0f5eedff17d5f6ff9a85b1686fe8ace7
Anton Nikolov <92668017+ANikolovvv@users.noreply.github.com>
2022-12-24 16:46:55 +0200

    Update app routing

--- SPA/src/app/app-routing.module.ts
+++ SPA/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import { AuthGuard } from './auth/guards/authGuard';
 
 import { CoachesComponent } from './coaches/coaches.component';
@@ -9,9 +9,12 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { OrderComponent } from './order/order.component';
 import { MyCourseComponent } from './my-course/my-course.component';
+import { BlogComponent } from './blog/blog.component';
+import { ArticleComponent } from './article/article.component';
 
 export const routes: Routes = [
   { path: "", pathMatch: 'full', redirectTo: '/home' },
+
   { path: 'auth', loadChildren: () => import("../app/auth/auth.module").then(m => m.AuthModule) },
   { path: 'home', component: HomeComponent },
   { path: 'gallery', component: GalleryComponent },
@@ -36,11 +39,19 @@ export const routes: Routes = [
       authRedirect: '/auth/login'
     }
   },
+
   {
     path: "order/:id", component: OrderComponent,
     canActivate: [AuthGuard],
     data: { authCuard: true, authRedirect: '/auth/login' }
   },
+  {
+    path: "blog",// component: BlogComponent,
+    loadChildren: () => import('./blog/blog.module').then(m => m.LazyModule)
+    //canActivate: [AuthGuard],
+    //data: { authCuard: true, authRedirect: '/auth/login' }
+  },
+ 
   { path: "not-found", component: NotFoundComponent },
 
   { path: "**", redirectTo: '/not-found' },
